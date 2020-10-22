75f4fb39f981274ae86d83869edbc2a50547ba6a
Jakub Gierus <jakub@gierus.ca>
2020-10-21 21:09:05 -0600

    10-20-2020

--- TeamCode/src/main/java/ca/webber/ftc/teleop/test/ControllerMotorTest.java
+++ TeamCode/src/main/java/ca/webber/ftc/teleop/test/ControllerMotorTest.java
@@ -4,22 +4,28 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "Debug: Controller Motor Test", group = "Debug")
 public class ControllerMotorTest extends OpMode {
     private DcMotorEx corehex = null;
-    private DcMotorEx hdhex = null;
+    private DcMotorEx hdhex1 = null;
+    private DcMotorEx hdhex2 = null;
 
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
 
         corehex = hardwareMap.get(DcMotorEx.class, "corehex");
-        hdhex = hardwareMap.get(DcMotorEx.class, "hdhex");
+        hdhex1 = hardwareMap.get(DcMotorEx.class, "hdhex1");
+        hdhex2 = hardwareMap.get(DcMotorEx.class, "hdhex2");
         corehex.setDirection(DcMotor.Direction.FORWARD);
-        hdhex.setDirection(DcMotor.Direction.FORWARD);
+        hdhex1.setDirection(DcMotor.Direction.FORWARD);
+        hdhex2.setDirection(DcMotor.Direction.REVERSE);
         corehex.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        hdhex.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        hdhex1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        hdhex2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
     }
 
     @Override
@@ -34,10 +40,13 @@ public class ControllerMotorTest extends OpMode {
     @Override
     public void loop() {
         double corePower = gamepad1.left_stick_y;
+
         double hdPower = gamepad1.right_trigger - gamepad1.left_trigger;
         corehex.setPower(corePower);
-        hdhex.setPower(hdPower);
+        hdhex1.setPower(hdPower);
+        hdhex2.setPower(hdPower);
         telemetry.addData("Motors", "corehex (%.2f) hdhex (%.2f)", corePower, hdPower);
+        telemetry.addData("Encoder Speeds", "hdhex1 %d hdhex2 %d", hdhex1.getCurrentPosition(), hdhex2.getCurrentPosition());
     }
 
     @Override
