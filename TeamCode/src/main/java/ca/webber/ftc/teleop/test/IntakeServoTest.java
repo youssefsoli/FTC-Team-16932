ea64dd9e08db8fac5aa2e72e4b8113701e51779f
Jakub Gierus <jakub@gierus.ca>
2020-11-02 16:40:22 -0700

    servo control 2

--- TeamCode/src/main/java/ca/webber/ftc/teleop/test/IntakeServoTest.java
+++ TeamCode/src/main/java/ca/webber/ftc/teleop/test/IntakeServoTest.java
@@ -1,19 +1,28 @@
 package ca.webber.ftc.teleop.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 
-public class DiskGathererUpper extends OpMode {
 
-    private Servo servoturn = null;
+@TeleOp(name = "Debug: Intake Servo Control", group = "Debug")
+public class IntakeServoTest extends OpMode {
+
+    private CRServo servoturn = null;
+    private DcMotorEx intaker = null;
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
 
-        servoturn = hardwareMap.get(Servo.class, "servoturn");
-        servoturn.setDirection(Servo.Direction.FORWARD);
+        servoturn = hardwareMap.get(CRServo.class, "servoturn");
+        intaker = hardwareMap.get(DcMotorEx.class, "intaker");
+        servoturn.setDirection(CRServo.Direction.FORWARD);
+        intaker.setDirection(DcMotorEx.Direction.FORWARD);
 
     }
 
@@ -28,12 +37,13 @@ public class DiskGathererUpper extends OpMode {
 
     @Override
     public void loop() {
-        boolean servoTurnOn = gamepad1.start;
-        if (servoTurnOn) {
-            servoturn.setPower(10);
+        if (gamepad1.a) {
+            servoturn.setPower(20);
+            intaker.setPower(20);
         }
         else {
-
+            servoturn.setPower(0);
+            intaker.setPower(0);
         }
     }
 
