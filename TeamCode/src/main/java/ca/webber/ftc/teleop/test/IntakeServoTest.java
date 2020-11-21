2a57da6520de4674cef82bac054e58f4b5b916a7
Youssef Soliman <youssefdevelops@gmail.com>
2020-11-21 09:50:45 -0700

    Modified intake test with servo

--- TeamCode/src/main/java/ca/webber/ftc/teleop/test/IntakeServoTest.java
+++ TeamCode/src/main/java/ca/webber/ftc/teleop/test/IntakeServoTest.java
@@ -2,27 +2,20 @@ package ca.webber.ftc.teleop.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 
 @TeleOp(name = "Debug: Intake Servo Control", group = "Debug")
 public class IntakeServoTest extends OpMode {
 
-    private CRServo servoturn = null;
-    private DcMotorEx intaker = null;
+    private Servo servo = null;
+
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
 
-        servoturn = hardwareMap.get(CRServo.class, "servoturn");
-        intaker = hardwareMap.get(DcMotorEx.class, "intaker");
-        servoturn.setDirection(CRServo.Direction.FORWARD);
-        intaker.setDirection(DcMotorEx.Direction.FORWARD);
+        servo = hardwareMap.get(Servo.class, "servo");
+        servo.setDirection(Servo.Direction.FORWARD);
 
     }
 
@@ -38,12 +31,9 @@ public class IntakeServoTest extends OpMode {
     @Override
     public void loop() {
         if (gamepad1.a) {
-            servoturn.setPower(20);
-            intaker.setPower(20);
-        }
-        else {
-            servoturn.setPower(0);
-            intaker.setPower(0);
+            servo.setPosition(1);
+        } else {
+            servo.setPosition(0);
         }
     }
 
